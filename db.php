c0e1db207876009b19197ecef2beda2fa12b2444
Clement <muhirwaclement@gmail.com>
2018-02-10 20:24:11 +0200

    ne tool
    this is okay

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php  
 error_reporting(E_ERROR | E_PARSE);
-$db = new mysqli("localhost", "root", "phpcoder" , "sawasawa");
+$db = new mysqli("localhost", "root", "clement123" , "sawasawa");
 	if($db->connect_errno){
 		die('Sorry we have some problem with the Database!');
 	}             
